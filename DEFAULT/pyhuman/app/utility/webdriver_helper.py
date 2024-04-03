da8beaae8a286928824bc7e277ccf04ce63458a8
Steven Lamp <vxn3kr@virginia.edu>
2024-04-03 10:27:28 -0400

    Updated for firefox

--- DEFAULT/pyhuman/app/utility/webdriver_helper.py
+++ DEFAULT/pyhuman/app/utility/webdriver_helper.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
 
 from .base_driver import BaseDriverHelper
 
-DRIVER_NAME = 'ChromeWebDriver'
+DRIVER_NAME = '/home/ubuntu/geckodriver'
 
 class WebDriverHelper(BaseDriverHelper):
 
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
     options.add_argument("--disable-gpu")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument("--start-maximized")
@@ -16,12 +16,12 @@ class WebDriverHelper(BaseDriverHelper):
     def __init__(self):
         super().__init__(name=DRIVER_NAME)
         self._driver_path = Service()
-        self._driver = webdriver.Chrome(self.options, self._driver_path)
+        self._driver = webdriver.Firefox(self.options, self._driver_path)
 
     @property
     def driver(self):
         return self._driver
-    
+
     def cleanup(self):
         self._driver.quit()
 
@@ -29,10 +29,9 @@ class WebDriverHelper(BaseDriverHelper):
 
     def check_valid_driver_connection(self):
         try:
-            driver = webdriver.Chrome(self._driver_path)
+            driver = webdriver.Firefox(self._driver_path)
             driver.quit()
             return True
         except Exception as e:
-            print('Could not load ChromeDriver: %s'.format(e))
+            print('Could not load geckodriver: %s'.format(e))
             return False
-
---
+++ install_reqs.sh
@@ -0,0 +1,6 @@
+#!\bin\bash 
+
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-selenium -y
+sudo apt-get install python3-bs4 -y
+
