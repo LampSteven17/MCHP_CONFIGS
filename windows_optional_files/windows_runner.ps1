9ba01f919674814c1324373cf19d040a697ecb8a
Steven Lamp <vxn3kr@virginia.edu>
2024-04-22 12:06:27 -0400

    Added windows Runner Script

---
+++ windows_optional_files/windows_runner.ps1
@@ -0,0 +1,24 @@
+#PS1 - Powershell Launch MITRE Caldera Human Plugin 
+
+$datey = Get-Date -Format "MM-dd-yyyy_HH-mm"
+$filey = $datey + "_mtx_mchp.txt"
+
+
+Get-Date -Format "MM-dd-yyyy_HH-mm" >> $filey
+"STARTING RECORDING SCRIPT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " >> $filey
+
+while ($true){ 
+
+        Get-Date -Format "MM-dd-yyyy_HH-mm" >> $filey
+        "STARTING MCHP PLUGIN - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " >> $filey
+        python3 human.py >> $filey
+
+        Stop-Process -Name "chrome" -Force >> $filey
+        Stop-Process -Name "mspaint" -Force >> $filey
+        Stop-Process -Name "soffice" -Force >> $filey
+
+        Get-ChildItem C:\Users\win11\Downloads\* | Remove-Item -recurse -force
+
+        Clear-RecycleBin -force
+
+}
